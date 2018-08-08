2514d3f44a29414c132a146da549b53fa8a21b82
april418 <apri.4.18@gmail.com>
2018-08-08 18:16:32 +0900

    add index page

---
+++ server/index.js
@@ -0,0 +1,33 @@
+// モジュールのインポート
+const express = require('express')
+const morgan = require('morgan')
+const { Client, middleware } = require('@line/bot-sdk')
+
+const lineConfig = {
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET
+}
+const bot = new Client(lineConfig)
+const server = express()
+
+server.use(morgan('common'))
+server.use(express.static('../public'))
+server.get('/', 'index.html')
+server.post('/webhook', middleware(lineConfig), (request, response, next) => {
+  response.sendStatus(200)
+
+  Promise.all(request.body.events.map((event) => {
+    if (event.type == "message" && event.message.type == "text"){
+      if (event.message.text == "こんにちは"){
+        events_processed.push(bot.replyMessage(event.replyToken, {
+          type: "text",
+          text: "これはこれは"
+        }))
+      }
+    }
+  })).then((response) => {
+    console.log(`${response.length} event(s) processed.`)
+  })
+})
+
+module.exports = server
