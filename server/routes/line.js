b1c0654b2f8fe2c5c37e0aa5195a8376df672ceb
april418 <apri.4.18@gmail.com>
2018-08-08 20:35:13 +0900

    add momir feature

--- server/routes/line.js
+++ server/routes/line.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const { Client, middleware } = require('@line/bot-sdk')
+const { card } = require('mtgsdk')
 
 const router = Router()
 const lineConfig = {
@@ -7,16 +8,56 @@ const lineConfig = {
   channelSecret: process.env.LINE_CHANNEL_SECRET
 }
 const bot = new Client(lineConfig)
+const pattern = /マナ|mana/
 
 router.post('/', middleware(lineConfig), (request, response, next) => {
   response.sendStatus(200)
 
   Promise.all(request.body.events.map((event) => {
-    if (event.type == "message" && event.message.type == "text"){
-      if (event.message.text == "こんにちは"){
+    if(event.type == "message" && event.message.type == "text") {
+      if(event.message.text == "こんにちは") {
         return bot.replyMessage(event.replyToken, {
           type: "text",
           text: "これはこれは"
+        })
+      }
+      else if(pattern.test(event.message.text)) {
+        return card.where({
+          layout: 'normal',
+          page: 1,
+          pageSize: 1,
+          type: 'Creature',
+          cmc: parseInt(event.message.text),
+          random: true
+        }).then(card => {
+          bot.replyMessage(event.replyToken, {
+            type: 'bubble',
+            header: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [
+                {
+                  type: 'text',
+                  text: card.name
+                }
+              ]
+            },
+            hero: {
+              type: 'image',
+              url: card.imageUrl,
+              size: 'full'
+            },
+            body: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [
+                {
+                  type: 'text',
+                  text: card.text
+                }
+              ]
+            }
+          })
         })
       }
     }
