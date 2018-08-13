cbe11c30f283f197a90e085c6fe0abaaa0c1991e
april418 <apri.4.18@gmail.com>
2018-08-13 19:11:22 +0900

    refactor momir skill

--- server/skills/bot/momir-basic.js
+++ server/skills/bot/momir-basic.js
@@ -3,7 +3,7 @@ const Skill = require('./skill.js')
 
 class MomirBasic extends Skill {
   static get PATTERN_MANACOST() {
-    return /マナ$|mana$/
+    return /\d{1,2}\s*マナ$|\d{1,2}\s*まな|\d{1,2}\s*mana$/i
   }
 
   constructor(bot, event) {
@@ -26,69 +26,84 @@ class MomirBasic extends Skill {
       random: true
     }).then(cards => {
       const card = cards[0]
-
       if(!card) return Promise.resolve()
-      return this.reply({
-        type: 'flex',
-        altText: card.name,
-        contents: {
-          type: 'bubble',
-          header: {
-            type: 'box',
-            layout: 'horizontal',
-            contents: [
-              {
-                type: 'text',
-                text: card.name,
-                align: 'start'
-              },
-              {
-                type: 'text',
-                text: card.manaCost,
-                align: 'end'
-              }
-            ]
-          },
-          body: {
-            type: 'box',
-            layout: 'vertical',
-            contents: [
-              {
-                type: 'text',
-                text: card.text,
-                size: 'sm',
-                wrap: true
-              }
-            ]
-          },
-          footer: {
-            type: 'box',
-            layout: 'horizontal',
-            contents: [
-              {
-                type: 'text',
-                text: card.power,
-                align: 'center'
-              },
-              {
-                type: 'text',
-                text: '/',
-                align: 'center'
-              },
-              {
-                type: 'text',
-                text: card.toughness,
-                align: 'center'
-              }
-            ]
-          }
-        }
-      })
+      return this.reply(this.generateMessage(card))
     })
   }
 
   messageMeansManaCost() {
     return this.constructor.PATTERN_MANACOST.test(this.event.message.text)
+  }
+
+  generateMessage(card) {
+    return {
+      type: 'flex',
+      altText: card.name,
+      contents: {
+        type: 'bubble',
+        header: this.generateMessageHeader(card),
+        body: this.generateMessageBody(card),
+        footer: this.generateMessageFooter(card)
+      }
+    }
+  }
+
+  generateMessageHeader(card) {
+    return {
+      type: 'box',
+      layout: 'horizontal',
+      contents: [
+        {
+          type: 'text',
+          text: card.name,
+          align: 'start'
+        },
+        {
+          type: 'text',
+          text: card.manaCost,
+          align: 'end'
+        }
+      ]
+    }
+  }
+
+  generateMessageBody(card) {
+    return {
+      type: 'box',
+      layout: 'vertical',
+      contents: [
+        {
+          type: 'text',
+          text: card.text,
+          size: 'sm',
+          wrap: true
+        }
+      ]
+    }
+  }
+
+  generateMessageFooter(card) {
+    return {
+      type: 'box',
+      layout: 'horizontal',
+      contents: [
+        {
+          type: 'text',
+          text: card.power,
+          align: 'center'
+        },
+        {
+          type: 'text',
+          text: '/',
+          align: 'center'
+        },
+        {
+          type: 'text',
+          text: card.toughness,
+          align: 'center'
+        }
+      ]
+    }
   }
 }
 
