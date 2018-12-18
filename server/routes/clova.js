37d32f84c99ec40de9e567e5925b8814b2c105ac
april418 <apri.4.18@gmail.com>
2018-12-18 12:18:23 +0900

    add ext

--- server/routes/clova.js
+++ server/routes/clova.js
@@ -1,12 +1,12 @@
 const { Router } = require('express')
 const { Middleware } = require('@line/clova-cek-sdk-nodejs')
-const skills = require('../skills/ext/skills.js')
+const skill = require('../skills/ext/sapporo-cafe.js')
 
 const router = Router()
 const clovaConfig = {
   applicationId: process.env.CLOVA_EXTENSION_ID,
 }
 
-router.post('/', Middleware(clovaConfig), ...skills)
+router.post('/', Middleware(clovaConfig), skill)
 
 module.exports = router
